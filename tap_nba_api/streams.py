6e592f86a88c05653940535a9fd4fe8a5c4ab173
Jon Adelson <gandalfthegraydelson@gmail.com>
2022-02-20 13:29:27 -0500

    Add `leaguegamelog` stream

--- tap_nba_api/streams.py
+++ tap_nba_api/streams.py
@@ -1,62 +1,51 @@
-"""Stream type classes for tap-nba-api."""
+from typing import Optional, Iterable
 
-from pathlib import Path
-from typing import Any, Dict, Optional, Union, List, Iterable
-
-from singer_sdk import typing as th  # JSON Schema typing helpers
+from singer_sdk import typing as th
 
 from tap_nba_api.client import NBAStatsStream
 
-# TODO: Delete this is if not using json files for schema definition
-SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
-# TODO: - Override `UsersStream` and `GroupsStream` with your own stream definition.
-#       - Copy-paste as many times as needed to create multiple stream types.
+from nba_api.stats.endpoints import leaguegamelog
 
 
-class UsersStream(NBAStatsStream):
-    """Define custom stream."""
-    name = "users"
-    path = "/users"
-    primary_keys = ["id"]
-    replication_key = None
-    # Optionally, you may also use `schema_filepath` in place of `schema`:
-    # schema_filepath = SCHEMAS_DIR / "users.json"
+class LeagueGameLogStream(NBAStatsStream):
+
+    name = "leaguegamelog"
+    primary_keys = ["GAME_ID", "TEAM_ID"]
+    replication_key = "GAME_DATE"
+
     schema = th.PropertiesList(
-        th.Property("name", th.StringType),
-        th.Property(
-            "id",
-            th.StringType,
-            description="The user's system ID"
-        ),
-        th.Property(
-            "age",
-            th.IntegerType,
-            description="The user's age in years"
-        ),
-        th.Property(
-            "email",
-            th.StringType,
-            description="The user's email address"
-        ),
-        th.Property("street", th.StringType),
-        th.Property("city", th.StringType),
-        th.Property(
-            "state",
-            th.StringType,
-            description="State name in ISO 3166-2 format"
-        ),
-        th.Property("zip", th.StringType),
+        th.Property("SEASON_ID", th.StringType),
+        th.Property("TEAM_ID", th.IntegerType),
+        th.Property("TEAM_ABBREVIATION", th.StringType),
+        th.Property("TEAM_NAME", th.StringType),
+        th.Property("GAME_ID", th.StringType),
+        th.Property("GAME_DATE", th.StringType),
+        th.Property("MATCHUP", th.StringType),
+        th.Property("WL", th.StringType),
+        th.Property("MIN", th.IntegerType),
+        th.Property("FGM", th.IntegerType),
+        th.Property("FGA", th.IntegerType),
+        th.Property("FG_PCT", th.NumberType),
+        th.Property("FG3M", th.IntegerType),
+        th.Property("FG3A", th.IntegerType),
+        th.Property("FG3_PCT", th.NumberType),
+        th.Property("FTM", th.IntegerType),
+        th.Property("FTA", th.IntegerType),
+        th.Property("FT_PCT", th.NumberType),
+        th.Property("OREB", th.IntegerType),
+        th.Property("DREB", th.IntegerType),
+        th.Property("REB", th.IntegerType),
+        th.Property("AST", th.IntegerType),
+        th.Property("STL", th.IntegerType),
+        th.Property("BLK", th.IntegerType),
+        th.Property("TOV", th.IntegerType),
+        th.Property("PF", th.IntegerType),
+        th.Property("PTS", th.IntegerType),
+        th.Property("PLUS_MINUS", th.IntegerType),
+        th.Property("VIDEO_AVAILABLE", th.IntegerType),
     ).to_dict()
 
-
-class GroupsStream(NBAStatsStream):
-    """Define custom stream."""
-    name = "groups"
-    path = "/groups"
-    primary_keys = ["id"]
-    replication_key = "modified"
-    schema = th.PropertiesList(
-        th.Property("name", th.StringType),
-        th.Property("id", th.StringType),
-        th.Property("modified", th.DateTimeType),
-    ).to_dict()
+    def get_records(self, context: Optional[dict]) -> Iterable[dict]:
+        records = leaguegamelog.LeagueGameLog().get_normalized_dict()["LeagueGameLog"]
+        for record in records:
+            yield record
--- tap_nba_api/tap.py
+++ tap_nba_api/tap.py
@@ -1,53 +1,22 @@
-"""NBAStats tap class."""
-
 from typing import List
 
 from singer_sdk import Tap, Stream
-from singer_sdk import typing as th  # JSON schema typing helpers
-# TODO: Import your custom stream types here:
-from tap_nba_api.streams import (
-    NBAStatsStream,
-    UsersStream,
-    GroupsStream,
-)
-# TODO: Compile a list of custom stream types here
-#       OR rewrite discover_streams() below with your custom logic.
+from singer_sdk import typing as th
+
+from tap_nba_api.streams import LeagueGameLogStream
+
 STREAM_TYPES = [
-    UsersStream,
-    GroupsStream,
+    LeagueGameLogStream,
 ]
 
 
 class TapNBAStats(Tap):
     """NBAStats tap class."""
+
     name = "tap-nba-api"
 
     # TODO: Update this section with the actual config values you expect:
-    config_jsonschema = th.PropertiesList(
-        th.Property(
-            "auth_token",
-            th.StringType,
-            required=True,
-            description="The token to authenticate against the API service"
-        ),
-        th.Property(
-            "project_ids",
-            th.ArrayType(th.StringType),
-            required=True,
-            description="Project IDs to replicate"
-        ),
-        th.Property(
-            "start_date",
-            th.DateTimeType,
-            description="The earliest record date to sync"
-        ),
-        th.Property(
-            "api_url",
-            th.StringType,
-            default="https://api.mysample.com",
-            description="The url for the API service"
-        ),
-    ).to_dict()
+    config_jsonschema = th.PropertiesList().to_dict()
 
     def discover_streams(self) -> List[Stream]:
         """Return a list of discovered streams."""
